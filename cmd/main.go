42234fe9143e1b74e6f782cb3f34615bcbc8228d
viana <flood.com@hotmail.com>
2025-03-25 02:05:32 -0300

    Se abituando com go

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/PING", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8080")
+}
